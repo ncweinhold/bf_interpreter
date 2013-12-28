462861c67d41eb6b3d014bad8d30952a6cb96dd0
Nick Weinhold <n.c.weinhold@googlemail.com>
2013-12-28 21:01:51 +0000

    Initial commit.
    For now this is just a Hello World application, but will eventually
    be a Brainfuck interpreter.

---
+++ main.c
@@ -0,0 +1,11 @@
+/**
+  * Brainfuck interpreter written by Nick Weinhold
+  */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("Hello World - this will be a brainfuck interpreter eventually\n");
+
+    return 0;
+}
