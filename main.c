cfd6b8b7af4015e649a90cf8813ce8723e53dbd0
Nick Weinhold <n.c.weinhold@googlemail.com>
2013-12-28 23:08:46 +0000

    Added first two Brainfuck instructions.
    Currently the interpreter processes only two instructions - '+' and
    '-'. Other characters are stripped out during the 'compile' phase.
    
    The Brainfuck program is hardcoded as +++++[]--- and is passed in
    to the BFInterpreter_compile procedure. As mentioned above, the
    non-recognised characters, in this case [] are stripped when the
    program is 'written to' the 'program tape'. Thus the program that
    is evaluated when the BFInterpreter_eval routine is called is
    +++++---
    
    By using the DEBUG flag during compilation, debug messages are
    printed out.

--- main.c
+++ main.c
@@ -3,9 +3,78 @@
   */
 
 #include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    unsigned short data[30000];
+    unsigned int data_ptr;
+
+    char program_tape[30000];
+    unsigned int pc;
+} BFInterpreter;
+
+void BFInterpreter_compile(BFInterpreter *interpreter, char *program);
+void BFInterpreter_dump_tape(BFInterpreter *interpreter);
+void BFInterpreter_eval(BFInterpreter *interpreter);
+
+void BFInterpreter_compile(BFInterpreter *interpreter, char *program) {
+
+#ifdef DEBUG
+    printf("Compiling the program: %s\n", program);
+#endif
+
+    int len = strlen(program);
+    int idx = 0;
+    int pc = 0;
+    while (idx != len) {
+
+#ifdef DEBUG
+        printf("Current token is: %c\n", program[idx]);
+#endif
+        if (program[idx] == '+') {
+            interpreter->program_tape[pc] = program[idx];
+            pc++;
+        }
+        if (program[idx] == '-') {
+            interpreter->program_tape[pc] = program[idx];
+            pc++;
+        }
+        idx++;
+    }
+}
+
+void BFInterpreter_dump_tape(BFInterpreter *interpreter) {
+    int idx = 0;
+    while (interpreter->program_tape[idx] != 0) {
+        printf("%c", interpreter->program_tape[idx]);
+        idx++;
+    }
+    printf("\n");
+}
+
+void BFInterpreter_eval(BFInterpreter *interpreter) {
+    while (interpreter->program_tape[interpreter->pc] != 0) {
+        char current_instruction = interpreter->program_tape[interpreter->pc];
+        if (current_instruction == '+') {
+            interpreter->data[interpreter->data_ptr]++;
+        }
+        if (current_instruction == '-') {
+            interpreter->data[interpreter->data_ptr]--;
+        }
+        interpreter->pc++;
+    }
+#ifdef DEBUG
+    printf("Item at data[0] is: %d\n", interpreter->data[0]);
+#endif
+}
 
 int main(int argc, char* argv[]) {
-    printf("Hello World - this will be a brainfuck interpreter eventually\n");
+    BFInterpreter interpreter;
+    BFInterpreter_compile(&interpreter, "+++++[]---");
+
+    BFInterpreter_eval(&interpreter);
+
+    BFInterpreter_dump_tape(&interpreter);
 
     return 0;
 }
