47575dc4bdb47a14b80b90cf8cdefe7fc84dc9e3
Nick Weinhold <n.c.weinhold@googlemail.com>
2013-12-29 21:54:45 +0000

    Added the loop instructions.
    Changed the representation of a Brainfuck instruction from a simple
    char to a tagged union - either a simple instruction like + or a
    loop instruction like [. This allows a simple way of keeping track
    of the jump locations where needed.
    
    Hardcoded a Hello World program for testing - worked correctly.
    Extra error checking will need to be put in place.

--- main.c
+++ main.c
@@ -8,17 +8,56 @@
 
 #define MAX_DATA_SIZE 30000
 #define MAX_TAPE_SIZE 30000
+#define MAX_STACK_SIZE 5000
+
+typedef enum {SIMPLE, JUMP, END} instruction_type;
+
+typedef struct {
+    instruction_type type;
+    union {
+        struct {
+            char opcode;
+        } SimpleInstruction;
+        struct {
+            char opcode;
+            unsigned int jump_location;
+        } JumpInstruction;
+    } Instruction;
+} BrainfuckInstruction;
+
+typedef struct {
+    unsigned int size;
+    unsigned int pc_values[MAX_STACK_SIZE];
+} Stack;
+
+void Stack_push(Stack *stack, unsigned int pc_value);
+unsigned int Stack_pop(Stack *stack);
+int Stack_empty(Stack *stack);
+
+void Stack_push(Stack *stack, unsigned int pc_value) {
+    stack->pc_values[stack->size] = pc_value;
+    stack->size++;
+}
+
+unsigned int Stack_pop(Stack *stack) {
+    stack->size--;
+    return stack->pc_values[stack->size];
+}
+
+int Stack_empty(Stack *stack) {
+    return (stack->size == 0);
+}
 
 typedef struct {
     unsigned short data[MAX_DATA_SIZE];
     unsigned int data_ptr;
 
-    char program_tape[MAX_TAPE_SIZE];
+    BrainfuckInstruction program_tape[MAX_TAPE_SIZE];
     unsigned int pc;
+    Stack jump_locations;
 } BFInterpreter;
 
 void BFInterpreter_compile(BFInterpreter *interpreter, char *program);
-void BFInterpreter_dump_tape(BFInterpreter *interpreter);
 void BFInterpreter_eval(BFInterpreter *interpreter);
 
 void BFInterpreter_compile(BFInterpreter *interpreter, char *program) {
@@ -49,42 +88,84 @@ void BFInterpreter_compile(BFInterpreter *interpreter, char *program) {
 
         if ((current_char == '+') || (current_char == '-') || (current_char == '>') 
             || (current_char == '<') || (current_char == '.') || (current_char == ',')) {
-            interpreter->program_tape[pc] = current_char;
+            interpreter->program_tape[pc].type = SIMPLE;
+            interpreter->program_tape[pc].Instruction.SimpleInstruction.opcode = current_char;
+            pc++;
+        }
+        if (current_char == '[') {
+            Stack_push(&interpreter->jump_locations, pc);
+            interpreter->program_tape[pc].type = JUMP;
+            interpreter->program_tape[pc].Instruction.JumpInstruction.opcode = current_char;
+            pc++;
+        }
+        if (current_char == ']') {
+            if (Stack_empty(&interpreter->jump_locations)) {
+                printf("Unmatched loop instruction!\n");
+                exit(1);
+            }
+            unsigned int jl = Stack_pop(&interpreter->jump_locations);
+            interpreter->program_tape[pc].type = JUMP;
+            interpreter->program_tape[pc].Instruction.JumpInstruction.opcode = current_char;
+            interpreter->program_tape[pc].Instruction.JumpInstruction.jump_location = jl;
+            interpreter->program_tape[jl].Instruction.JumpInstruction.jump_location = pc;
+#ifdef DEBUG
+            printf("Instruction at program_tape[%d] will jump to location: %d\n", pc, jl);
+            printf("Instruction at program_tape[%d] will jump to location: %d\n", jl, pc);
+#endif
             pc++;
         }
         idx++;
     }
-}
-
-void BFInterpreter_dump_tape(BFInterpreter *interpreter) {
-    int idx = 0;
-    while (interpreter->program_tape[idx] != 0) {
-        printf("%c", interpreter->program_tape[idx]);
-        idx++;
+    if (!Stack_empty(&interpreter->jump_locations)) {
+        printf("Unmatched loop instruction!\n");
+        exit(1);
     }
-    printf("\n");
+    interpreter->program_tape[pc].type = END;
 }
 
 void BFInterpreter_eval(BFInterpreter *interpreter) {
-    while (interpreter->program_tape[interpreter->pc] != 0) {
-        char current_instruction = interpreter->program_tape[interpreter->pc];
-        if (current_instruction == '+') {
+    while (interpreter->program_tape[interpreter->pc].type != END) {
+        BrainfuckInstruction current_instruction = interpreter->program_tape[interpreter->pc];
+        char current_opcode;
+        if (current_instruction.type == SIMPLE) {
+            current_opcode = current_instruction.Instruction.SimpleInstruction.opcode;
+        }
+        if (current_instruction.type == JUMP) {
+            current_opcode = current_instruction.Instruction.JumpInstruction.opcode;
+        }
+        if (current_opcode == '+') {
             interpreter->data[interpreter->data_ptr]++;
         }
-        if (current_instruction == '-') {
+        if (current_opcode == '-') {
             interpreter->data[interpreter->data_ptr]--;
         }
-        if (current_instruction == '>') {
+        if (current_opcode == '>') {
             interpreter->data_ptr++;
         }
-        if (current_instruction == '<') {
+        if (current_opcode == '<') {
             interpreter->data_ptr--;
         }
-        if (current_instruction == '.') {
+        if (current_opcode == '.') {
             putchar(interpreter->data[interpreter->data_ptr]);
         }
-        if (current_instruction == ',') {
+        if (current_opcode == ',') {
             interpreter->data[interpreter->data_ptr] = getchar();
+        }
+        if (current_opcode == ']') {
+            if (interpreter->data[interpreter->data_ptr]) {
+                interpreter->pc = current_instruction.Instruction.JumpInstruction.jump_location;
+#ifdef DEBUG
+                printf("Jumping back to location: %d\n", interpreter->pc);
+#endif
+            }
+        }
+        if (current_opcode == '[') {
+            if (!interpreter->data[interpreter->data_ptr]) {
+                interpreter->pc = current_instruction.Instruction.JumpInstruction.jump_location;
+#ifdef DEBUG
+                printf("Jumping forward to location: %d\n", interpreter->pc);
+#endif
+            }
         }
         interpreter->pc++;
     }
@@ -95,9 +176,8 @@ void BFInterpreter_eval(BFInterpreter *interpreter) {
 
 int main(int argc, char* argv[]) {
     BFInterpreter interpreter;
-    BFInterpreter_compile(&interpreter, "++++,..>+++[]--<-");
+    BFInterpreter_compile(&interpreter, "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
     BFInterpreter_eval(&interpreter);
 
-    printf("\n");
     return 0;
 }
